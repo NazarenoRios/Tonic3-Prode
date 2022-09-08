938a99bee59df77fc16bdbf78f48192fa6ca0ed0
Fredyrooh <fredyrooh@gmail.com>
2022-09-08 19:49:32 -0300

    services were created for players

---
+++ server/services/player.services.js
@@ -0,0 +1,64 @@
+const { Op } = require("sequelize")
+const Player = require("../models/Player")
+
+class PlayerServices{
+    static async createPlayer (body){
+        try{
+           return await Player.create(body)
+        } catch(error){
+            console.log(error)
+        }
+    }
+
+    static async getAllPlayers (){
+        try{
+           return await Player.findAll()
+        } catch(error){
+            console.log(error)
+        }
+    }    
+
+    static async findByid(id){
+        try{
+           return await Player.findByPk(id)
+            
+        } catch(error){
+            console.log(error)
+        }
+    }
+
+    static async findByName(name){
+        try{
+           return await Player.findAll({
+            where:{name: {
+                [Op.iLike]: `%${name}%`
+            }}
+           })
+            
+        } catch(error){
+            console.log(error)
+        }
+    }
+
+    static async modifyPlayer(player,{fullname,img,info,goals}){
+        try{
+            player.fullname = fullname
+            player.img = img
+            player.info = info
+            player.goals = goals
+        return await Player.save()
+        } catch(error){
+            console.log(error)
+        }
+    }
+
+    static async deletePlayer(id){
+        try{
+          return await Player.destroy({ where:{id} })       
+        } catch(error){
+           return res.sendStatus(500).json({message:error.message})
+        }
+    }
+}
+
+module.exports = PlayerServices
