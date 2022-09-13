8aaf4855ece7ca8d1405f65d823a4a215fe9661d
GYauck <yauckgaston@gmail.com>
2022-09-13 10:42:38 -0300

    Geolocalizacion(geoIp), falta completar

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const { createHmac } = require("node:crypto");
 const mailer = require("../utils/mailer");
 
+const geoip = require('geoip-lite');
+
 const { OAuth2Client } = require("google-auth-library");
 const router = require("../routes");
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
@@ -41,6 +43,10 @@ exports.googlelogin = (req, res) => {
               lastname: user.lastname,
               admin: user.admin,
               isVerified: user.isVerified,
+              phone: user.phone,
+              state: user.state,
+              city: user.city,
+              address: user.address,
             };
             const token = tokens.generateToken(payload);
             // sessionStorage.setItem("token", token)
@@ -84,149 +90,37 @@ exports.register = (req, res) => {
   });
 };
 
-exports.verifyEmail = async (req, res) => {
+exports.verifyEmail = async(req, res) => {
   try {
-    const { emailToken } = req.params;
-    const user = await User.findOne({ where: { emailToken: emailToken } });
-    if (user) {
-      User.update(
-        { isVerified: true, emailToken: null },
-        { where: { emailToken: emailToken } }
-      );
-      res.redirect("http://localhost:3000/");
-      res.sendStatus(204);
+    const { emailToken } = req.params
+    const user = await User.findOne({ where: { emailToken: emailToken } })
+    if (user){
+      User.update({ isVerified: true, emailToken: null },{ where: { emailToken: emailToken } })
+      res.redirect("http://localhost:3000/")
+      res.sendStatus(204)
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
 exports.validation = (req, res) => {
-  res.send(req.user);
+  res.send(req.user)
 };
 
 exports.logout = (req, res) => {
-  res.clearCookie("token");
-  res.sendStatus(204);
+  res.clearCookie("token")
+  res.sendStatus(204)
 };
 
-//REGISTRO
-
-/* exports.register = (req, res) => {
-  const userData = req.body;
-  User.findOne({ where: { email: req.body.email } }).then((user) => {
-    if (!user) {
-      User.create(userData)
-      .then(() => res.sendStatus(201));
-    } else {
-      res.send({ message: "Usuario ya registrado" });
-    }
-  });
-}; */
-
-//GOOGLELOGIN
-
-/* const { OAuth2Client } = require("google-auth-library");
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-
-exports.googlelogin = (req, res) => {
-  const { credential } = req.body;
-
-  client
-    .verifyIdToken({
-      idToken: credential,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    })
-    .then((userInfo) => {
-      const { email, name } = userInfo.payload;
-      const password = credential;
-      User.findOne({
-        where: { email: email },
-      }).then((user) => {
-        if (!user) {
-          return User.create({
-            email: email,
-            password: password,
-            fullname: name,
-            emailToken: createHmac("sha256", process.env.SECRET).update("vamo la escaloneta").digest("hex"),
-            isVerified: false,
-            admin: false
-          }).then((user) => {
-            user.validatePassword(password).then((isValid) => {
-              if (!isValid) return res.send(401);
-
-              const payload = {
-                id: user.id,
-                email: user.email,
-                fullname: user.fullname,
-                admin: user.admin,
-              };
-              const token = tokens.generateToken(payload);
-              res.cookie("token", token);
-              res.sendStatus(201);
-            });
-          });
-        }
-        user.validatePassword(password).then((isValid) => {
-          if (!isValid) return res.send(401);
-          const payload = {
-            id: user.id,
-            email: user.email,
-            fullname: user.fullname,
-            admin: user.admin
-          };
-          const token = tokens.generateToken(payload);
-          res.cookie("token", token);
-          res.sendStatus(201);
-        });
-      });
-    });
-};
-
-exports.validation = (req, res) => {
-    res.send(req.user);
-};
-
-exports.logout = (req, res) => {
-    res.clearCookie("token");
-    res.sendStatus(204);
-}; */
-
-/* exports.googlelogin = (req, res) => {
-  const { credential } = req.body;
-  client
-    .verifyIdToken({
-      idToken: credential,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    })
-    .then((userInfo) => {
-      const { email, name } = userInfo.payload;
-      const password = credential;
-      User.findOne({
-        where: { email: email },
-      }).then((user) => {
-        if (!user) {
-          return User.create({
-            email: email,
-            password: password,
-            fullname: name,
-            emailToken: createHmac("sha256", process.env.SECRET).update(`${email}`).digest("hex"),
-            isVerified: false,
-            admin: false
-          }).then((user) => mailer.sendMail(user.dataValues.id).then((result)=> console.log("email enviado", result)));
-        }
-        user.validatePassword(password).then((isValid) => {
-          if (!isValid) return res.send(401);
-          const payload = {
-            id: user.id,
-            email: user.email,
-            fullname: user.fullname,
-            admin: user.admin
-          };
-          const token = tokens.generateToken(payload);
-          res.cookie("token", token);
-          res.sendStatus(201);
-        });
-      });
-    });
-}; */
+exports.showIP = async (req, res) => {
+  try {
+  const geo = geoip.lookup(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+  console.log(req.ip)
+  res.json({country: geo.country});
+  }
+  catch(error) {
+    console.log(error)
+  }
+  
+}
