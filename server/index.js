21498b468e51b8369e40f3d88886431dde49b202
GYauck <yauckgaston@gmail.com>
2022-09-30 02:58:42 -0300

    agregado de swagger

--- server/index.js
+++ server/index.js
@@ -2,6 +2,25 @@
 const express = require("express");
 const path = require("path")
 
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerSpec = {
+  definition : {
+      openapi : "3.0.0",
+      info : {
+          title : "Node Prode API",
+          version : "1.0.0"
+      },
+  servers : [
+      {
+          url : "http://localhost:3001/"
+      }
+  ]
+  },
+apis : [`${path.join(__dirname, "./routes/*.js")}`], 
+}
+
 require("dotenv").config()
 
 const models = require("./models")
@@ -25,6 +44,7 @@ const metrics_routes=require('./metrics/routes')
 // Express Routing
 app.use("/api", routes)
 app.use('/metrics',metrics_routes)
+app.use("/api-doc",swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
 
 db.sync({ force: false }).then(() => {
  // start_metrics()
@@ -33,4 +53,3 @@ db.sync({ force: false }).then(() => {
     console.log(`Server listening at port ${process.env.PORT}`);
   });
 });
-
--- server/routes/award.js
+++ server/routes/award.js
@@ -3,15 +3,168 @@ const AwardController = require("../controllers/award.controller")
 const { validate_admin } = require("../middleware/validate_admin")
 const router = express.Router()
 
+
+/**
+*  @swagger
+*  components:
+*  schemas:
+*   Award:
+*     type : object
+*     properties:
+*       place:
+*         type: integer
+*         description : place of the winner
+*       name : 
+*         type : string
+*         description: name of the award
+*       img:
+*         type : string
+*         description: image of the award
+*     required:
+*       - place
+*       - name
+*       - img
+*     example:
+*       place: 1
+*       name: House
+*       img : https://img.freepik.com/foto-gratis/casa-aislada-campo_1303-23773.jpg?w=2000
+*/
+
+
+
+
+
 //ruta para crear un premio
 router.post("/create",validate_admin,AwardController.createAward)
+/**
+*   @swagger
+*  /api/award/create:
+*   post:
+*     sumary: create a new award
+*     tags: [Award]
+*     requestBody:
+*       required: true
+*       content:
+*         aplication/json:
+*           schema:
+*             type: string
+*             $ref: '#components/schemas/Award'
+*     responses:
+*       201:
+*         description: new award created!
+*/
+
 //ruta para traer todos los premios
 router.get("/all",AwardController.getAllAward)
+/**
+*   @swagger
+*  /api/award/all:
+*   get:
+*     sumary: get all awards
+*     tags: [Award]
+*     requestBody:
+*       required: false
+*       content:
+*         aplication/json:
+*           schema:
+*             type: string
+*             $ref: '#components/schemas/Award'
+*     responses:
+*       200:
+*         description: all awards founded!
+*         content:
+*           application/json:
+*            schema:
+*             type: object
+*             $ref: '#/components/schemas/Award'
+*       404: 
+*         description: awards not found
+*/
+
+
+
 //ruta para encontrar un premio por id
 router.get("/:id",AwardController.findByid)
+/**
+*  @swagger
+*  /api/award/{id}:
+*   get:
+*     sumary: get an award by id
+*     tags: [Award]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*          type: string
+*         required: true
+*         description: the award id
+*     responses:
+*       200:
+*         description: award found!
+*         content:
+*           application/json:
+*            schema:
+*             type: object
+*             $ref: '#/components/schemas/Award'
+*       404: 
+*         description: award not found
+*/
+
+
+
 //ruta para modificar un premio
 router.put("/modify/:id",validate_admin,AwardController.modifyAward)
+/**
+*  @swagger
+*  /api/award/{id}:
+*   put:
+*     sumary: modify a award by id
+*     tags: [Award]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*          type: string
+*         required: true
+*         description: the award id
+*     responses:
+*       200:
+*         description: award modified!
+*         content:
+*           application/json:
+*            schema:
+*             type: object
+*             $ref: '#/components/schemas/Award'
+*       404: 
+*         description: award not found
+*/
+
+
 //ruta para eliminar un premio
 router.delete("/:id",validate_admin,AwardController.deleteAward)
+/**
+*  @swagger
+*  /api/award/{id}:
+*   delete:
+*     sumary: delete a award by id
+*     tags: [Award]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*          type: string
+*         required: true
+*         description: the award id
+*     responses:
+*       200:
+*         description: award deleted!
+*         content:
+*           application/json:
+*            schema:
+*             type: object
+*             $ref: '#/components/schemas/Award'
+*       404: 
+*         description: award not found
+*/
 
 module.exports = router
